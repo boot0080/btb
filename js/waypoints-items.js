28deaa95b64973bc6a282e9ecac82682fdeb16da
Daniela <just@addsalt.ca>
2015-03-08 13:51:28 -0400

    feat(all): add waypoints animation for top tabs on index

---
+++ js/waypoints-items.js
@@ -0,0 +1,7 @@
+var $win = $(window);
+var $navTop = $('.navtop-hid');
+var $areaTrigger = $('.area-trigger');
+
+$areaTrigger.waypoint(function() {
+	$navTop.addClass('js-navtop-fade');
+}, { offset:'10%' });
